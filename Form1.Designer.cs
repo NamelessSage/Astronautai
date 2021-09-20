6194a3b13866aedb1389385eef9d3c314f59d39e
WeDDo <mantuxas001@gmail.com>
2021-09-20 13:32:09 +0300

    basic communication with azure server

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,6 +32,8 @@ namespace Astronautai
             this.components = new System.ComponentModel.Container();
             this.astroDataSet = new Astronautai.AstroDataSet();
             this.astroDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.astroDataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.astroDataSetBindingSource)).BeginInit();
             this.SuspendLayout();
@@ -46,22 +48,47 @@ namespace Astronautai
             this.astroDataSetBindingSource.DataSource = this.astroDataSet;
             this.astroDataSetBindingSource.Position = 0;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(138, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             ((System.ComponentModel.ISupportInitialize)(this.astroDataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.astroDataSetBindingSource)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.BindingSource astroDataSetBindingSource;
         private AstroDataSet astroDataSet;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,14 +13,114 @@ namespace Astronautai
 {
     public partial class Form1 : Form
     {
+        //define the connection string of azure database.
+        string cnString = "Server=tcp:astronauts.database.windows.net,1433;Initial Catalog=Astro;Persist Security Info=False;User ID=astronautas;Password=Batonas5;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        bool access = true;
+        int playerId = 1;
+
+        void OnChange(object sender, SqlNotificationEventArgs e)
+        {
+
+        }
+
         public Form1()
         {
             InitializeComponent();
+            KeyPreview = true;
         }
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
 
+            //Console.WriteLine("hello!");
+            //AstroDataSet1 db = new AstroDataSet1();
+
+            //db.Players.AddPlayersRow("0", "1", "Hello!");
+            //PutData();
+
+            GetData();
+        }
+
+        private async void PutData(string X, string Y, string Input)
+        {
+            //define the insert sql command, here I insert data into the student table in azure db.
+            string cmdText = @"update players set ""input""=@input where id=1";
+
+            using (SqlConnection con = new SqlConnection(cnString))
+            using (SqlCommand cmd = new SqlCommand(cmdText, con))
+            {
+                con.Open();
+                cmd.Parameters.AddWithValue("@X", X);
+                cmd.Parameters.AddWithValue("@Y", Y);
+                cmd.Parameters.AddWithValue("@input", Input);
+
+                cmd.ExecuteNonQuery();
+
+                con.Close();
+            }
+
+            Console.WriteLine("completed***");
+            //GetData();
+        }
+
+        private void GetData()
+        {
+            //define the insert sql command, here I insert data into the student table in azure db.
+            string cmdText = @"select * from Players where id=" + playerId;
+
+            using (SqlConnection con = new SqlConnection(cnString))
+            using (SqlCommand cmd = new SqlCommand(cmdText, con))
+            {
+                con.Open();
+                using (SqlDataReader dataReader = cmd.ExecuteReader())
+                {
+                    SqlDependency dependency = new SqlDependency(cmd);
+                    dependency.OnChange += OnChange;
+                    while (dataReader.Read())
+                    {
+                        for (int i = 0; i < dataReader.FieldCount; i++)
+                        {
+                            if (dataReader.IsDBNull(i))
+                            {
+                                access = false;
+                            } 
+                        }
+
+                        access = true;
+                        string X = dataReader.GetString(1);
+                        string Y = dataReader.GetString(2);
+                        string Input = dataReader.GetString(3);
+
+                        label1.Text = Input;
+
+                        Console.WriteLine(X + " " + Y + " " + Input);
+                    }
+                }
+                con.Close();
+            }
+
+            Console.WriteLine("completed***");
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            Console.WriteLine(e.KeyCode);
+            if(e.KeyCode == Keys.W)
+            {
+                PutData("1", "1", "W");
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                PutData("1", "1", "S");
+            }
+            if (e.KeyCode == Keys.A)
+            {
+                PutData("1", "1", "A");
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                PutData("1", "1", "D");
+            }
         }
     }
 }
