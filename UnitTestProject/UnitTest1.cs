7de2c33cb867a816ba455c69a2b9f1c7fc67179f
Descory <simas0315@gmail.com>
2021-11-14 18:46:01 +0200

    50.3%

--- UnitTestProject/UnitTest1.cs
+++ UnitTestProject/UnitTest1.cs
@@ -13,16 +13,25 @@ namespace UnitTestProject
         [TestMethod]
         public void TestPlayerCanMoveTrue()
         {
-            Player player = createTestPlayer();
-            Player playerMove = gameData.PlayerCanMove(createTestPlayer());
+            Player player = createTestPlayer(100, 100, 'W');
+            Player playerMove = gameData.PlayerCanMove(createTestPlayer(100, 100, 'W'));
 
             Assert.AreEqual(player.Y - player.Speed, playerMove.Y);
+            playerMove = gameData.PlayerCanMove(createTestPlayer(100, 100, 'A'));
+            Assert.AreEqual(player.X - player.Speed, playerMove.X);
+            playerMove = gameData.PlayerCanMove(createTestPlayer(100, 100, 'S'));
+            Assert.AreEqual(player.Y + player.Speed, playerMove.Y);
+            playerMove = gameData.PlayerCanMove(createTestPlayer(100, 100, 'D'));
+            Assert.AreEqual(player.X + player.Speed, playerMove.X);
         }
         [TestMethod]
         public void TestCheckCollisionPlayersTrue()
         {
-            Player player = createTestPlayer();
-            Assert.AreEqual(true, gameData.CheckCollisionPlayers(player));
+            Player player = createTestPlayer(10, 10, 'W');
+            Player player1 = createTestPlayer(15, 15, 'A');
+            gameData.AddPlayer(player);
+            gameData.AddPlayer(player1);
+            Assert.AreEqual(false, gameData.CheckCollisionPlayers(player));
         }
         [TestMethod]
         public void TestCheckCollisionEnemyTrue()
@@ -34,23 +43,23 @@ namespace UnitTestProject
         [TestMethod]
         public void TestCheckNotCollisionPickup()
         {
-            Pickup pickup = createTestPickup(10,10);
+            Pickup pickup = createTestPickup(100,100);
             Assert.AreEqual(true, gameData.CheckNotCollisionPickup(pickup));
-            pickup = createTestPickup(100, 100);
+            pickup = createTestPickup(10, 10);
             Assert.AreEqual(false, gameData.CheckNotCollisionPickup(pickup));
         }
         [TestMethod]
         public void TestAddPlayerAndGetPlayer()
         {
-            Player player = createTestPlayer();
+            Player player = createTestPlayer(10, 10, 'W');
             gameData.AddPlayer(player);
-            Assert.AreEqual(player, gameData.GetPlayers()[0]);
+            Assert.AreEqual(player, gameData.GetPlayers()[1]);
         }
 
         [TestMethod]
         public void TestAddProjectileAndGetProjectile()
         {
-            Projectile projectile = createTestProjectile(10,10);
+            Projectile projectile = createTestProjectile(10,10, 'W');
             gameData.AddProjectile(projectile);
             Assert.AreEqual(projectile, gameData.GetProjectiles()[0]);
         }
@@ -58,9 +67,21 @@ namespace UnitTestProject
         [TestMethod]
         public void TestUpdateProjectilesCoords()
         {
-            Projectile projectile = createTestProjectile(10, 10);
+            Projectile projectile = createTestProjectile(10, 10, 'W');
+            Projectile projectile1 = createTestProjectile(10, 10, 'A');
+            Projectile projectile2 = createTestProjectile(10, 10, 'S');
+            Projectile projectile3 = createTestProjectile(10, 10, 'D');
+            gameData.AddProjectile(projectile1);
+            gameData.AddProjectile(projectile2);
+            gameData.AddProjectile(projectile3);
             gameData.UpdateProjectileCoords();
+            projectile1 = createTestProjectile(10, 10, 'A');
+            projectile2 = createTestProjectile(10, 10, 'S');
+            projectile3 = createTestProjectile(10, 10, 'D');
             Assert.AreEqual(projectile.Y - 10, gameData.GetProjectiles()[0].Y);
+            Assert.AreEqual(projectile1.X - 10, gameData.GetProjectiles()[1].X);
+            Assert.AreEqual(projectile2.Y + 10, gameData.GetProjectiles()[2].Y);
+            Assert.AreEqual(projectile3.X + 10, gameData.GetProjectiles()[3].X);
         }
         [TestMethod]
         public void TestCheckMapEdge()
@@ -80,12 +101,32 @@ namespace UnitTestProject
             List<Obstacle> obs = new List<Obstacle>();
             Assert.AreNotEqual(obs, gameData.GetObstacles());
         }
-
-        public Player createTestPlayer()
+        [TestMethod]
+        public void TestUpdatePlayer()
         {
-            Player player = new Player("test", 3, 10, 25, 16);
-            player.SetCoordinates(100, 100);
-            player.Rotation = 'W';
+            Player player = createTestPlayer(10, 10, 'W');
+            gameData.UpdatePlayer(player);
+            Assert.AreEqual(player, gameData.GetPlayers()[0]);
+        }
+        [TestMethod]
+        public void TestGetMap()
+        {
+            Assert.IsNotNull(gameData.GetMap());
+        }
+        [TestMethod]
+        public void TestAddAsteroidAndUpdateAsteroidCoords()
+        {
+            gameData.AddPlayer(createTestPlayer(10, 10, 'W'));
+            gameData.AddAsteroid();
+            gameData.UpdateAsteroidCoords();
+            //Assert.IsNotNull(gameData.GetEnemies());
+        }
+
+        public Player createTestPlayer(int x, int y, char rot)
+        {
+            Player player = new Player("test"+x.ToString(), 3, 10, 25, 16);
+            player.SetCoordinates(x, y);
+            player.Rotation = rot;
             return player;
         }
         public Enemy createTestEnemy()
@@ -106,12 +147,12 @@ namespace UnitTestProject
             return pickup;
         }
 
-        public Projectile createTestProjectile(int x, int y)
+        public Projectile createTestProjectile(int x, int y, char dir)
         {
             Projectile projectile = new Projectile();
             projectile.X = x;
             projectile.Y = y;
-            projectile.Direction = 'W';
+            projectile.Direction = dir;
             return projectile;
         }
     }
