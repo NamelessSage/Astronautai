4ff4371406d44ea6b58db6cabe6f1822ba90a9a4
Xricore <61002997+Xricore@users.noreply.github.com>
2021-12-05 18:24:05 +0200

    fff

---
+++ Astronautai/Classes/Visitor/Visitor1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Astronautai.Classes.Visitor
+{
+    public class Visitor1 : IVisitor
+    {
+        public void Visit()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Astronautai/Classes/Visitor/Visitor2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Astronautai.Classes.Visitor
+{
+    class Visitor2 : IVisitor
+    {
+        public void Visit()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
